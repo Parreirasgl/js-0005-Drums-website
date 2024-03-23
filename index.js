3568fa026d21c9fcc9ef8682d692a760ad205641
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-03-23 15:19:27 -0300

    added audio to letters

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberOfDrumButtons; i++) {
 
-    document.querySelectorAll(".drum") [i].addEventListener("click", function () {alert("I got clicked!");});
+    document.querySelectorAll(".drum") [i].addEventListener("click", function () {
+
+        var audio = new Audio("sounds/tom-1.mp3"); 
+        audio.play();
+
+    
+    });
 }
